172ab5fd60b46a1ca4579018a0bb6402155d2238
minhuongquang <uongquangminh1803@gmail.com>
2023-08-09 15:44:30 +0700

    AddShoppingCartViewComponent

--- BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -20,15 +20,6 @@ namespace BulkyWeb.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
-            var claimsIdentity = (ClaimsIdentity)User.Identity;
-            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
-
-            if(claim != null)
-            {
-                HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).Count());
-            }
-
-
             List<Product> products = _unitOfWork.Product.GetAll(incluedeProperties:"Category").ToList();
             return View(products);
         }
